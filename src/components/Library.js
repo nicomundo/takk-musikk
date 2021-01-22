18b05c08c554b474f71c011798aa3aa04968dadb
Jake Berg <jacobvictorberg@gmail.com>
2021-01-22 15:30:41 -0500

    secret change

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,35 +2,36 @@ import React from "react";
 import LibrarySong from "./LibrarySong";
 
 const Library = ({
-	songs,
-	setCurrentSong,
-	audioRef,
-	isPlaying,
-	setSongs,
-	libraryStatus,
-	setLibraryStatus,
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+  setLibraryStatus,
 }) => {
-	return (
-		<div className={`library ${libraryStatus ? "active-library" : ""} `}>
-			<div className="library-nav">
-				<h2>Library</h2>
-				<button onClick={() => setLibraryStatus(!libraryStatus)}>Exit</button>
-			</div>
-			<div className="library-songs">
-				{songs.map((song) => (
-					<LibrarySong
-						isPlaying={isPlaying}
-						songs={songs}
-						song={song}
-						setCurrentSong={setCurrentSong}
-						id={song.id}
-						key={song.id}
-						audioRef={audioRef}
-						setSongs={setSongs}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  // secret change
+  return (
+    <div className={`library ${libraryStatus ? "active-library" : ""} `}>
+      <div className="library-nav">
+        <h2>Library</h2>
+        <button onClick={() => setLibraryStatus(!libraryStatus)}>Exit</button>
+      </div>
+      <div className="library-songs">
+        {songs.map((song) => (
+          <LibrarySong
+            isPlaying={isPlaying}
+            songs={songs}
+            song={song}
+            setCurrentSong={setCurrentSong}
+            id={song.id}
+            key={song.id}
+            audioRef={audioRef}
+            setSongs={setSongs}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Library;
