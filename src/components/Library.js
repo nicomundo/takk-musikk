df0f7ecb8e5a8636f9497e98867074c4f14d44ac
Nicomundo <nickbartleberg@gmail.com>
2021-01-22 12:20:17 -0800

    first commit

---
+++ src/components/Library.js
@@ -0,0 +1,36 @@
+import React from "react";
+import LibrarySong from "./LibrarySong";
+
+const Library = ({
+	songs,
+	setCurrentSong,
+	audioRef,
+	isPlaying,
+	setSongs,
+	libraryStatus,
+	setLibraryStatus,
+}) => {
+	return (
+		<div className={`library ${libraryStatus ? "active-library" : ""} `}>
+			<div className="library-nav">
+				<h2>Library</h2>
+				<button onClick={() => setLibraryStatus(!libraryStatus)}>Exit</button>
+			</div>
+			<div className="library-songs">
+				{songs.map((song) => (
+					<LibrarySong
+						isPlaying={isPlaying}
+						songs={songs}
+						song={song}
+						setCurrentSong={setCurrentSong}
+						id={song.id}
+						key={song.id}
+						audioRef={audioRef}
+						setSongs={setSongs}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+export default Library;
